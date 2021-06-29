2b8a5a85b0a5ad12d78486d6c31a6391b5b6bf8f
Olivier M <42801943+gqdc@users.noreply.github.com>
2021-06-29 18:55:42 +0200

    Create renew.sh

---
+++ renew.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+SYSADMIN_EMAIL="user@domain.tld"
+
+# Define IFS to a "space" character, to parse $RENEWED_DOMAINS which has the format "domain1.tld domain2.tld"
+IFS=' '
+
+for domain in $RENEWED_DOMAINS; do
+    # move to the correct let's encrypt domain's directory
+    cd $RENEWED_LINEAGE
+    # cat fullchain.pem and privkey.pem files to make a combined $domain.pem file for haproxy
+    cat fullchain.pem privkey.pem > /etc/ssl/private/$domain.pem
+done
+
+# reload haproxy to load the new certificate(s)
+systemctl reload haproxy
+
+# Send an e-mail to $SYSADMIN_EMAIL to tell him/her if Haproxy reloaded successfully
+if test $? -eq 0
+then
+    echo "Haproxy successfully reloaded" |mail -s "[CERTBOT] ${domain} : certificate renewed" ${SYSADMIN_EMAIL}
+else
+    echo "Haproxy not reloaded" |mail -s "[CERTBOT] ${domain} : certificate not renewed" ${SYSADMIN_EMAIL}
+fi
