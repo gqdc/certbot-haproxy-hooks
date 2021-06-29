8b39d4c190d81cac5779082e2d0c5ce647227f24
Olivier M <42801943+gqdc@users.noreply.github.com>
2021-06-29 18:58:57 +0200

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# Sysadmin e-mail
 SYSADMIN_EMAIL="user@domain.tld"
-
+# The folder where to store the final .pem ( which containt the sum of privkey.pem and fullchain.pem)
+CRT_FOLDER="/etc/ssl/private"
 # Define IFS to a "space" character, to parse $RENEWED_DOMAINS which has the format "domain1.tld domain2.tld"
 IFS=' '
 
@@ -8,7 +10,7 @@ for domain in $RENEWED_DOMAINS; do
     # move to the correct let's encrypt domain's directory
     cd $RENEWED_LINEAGE
     # cat fullchain.pem and privkey.pem files to make a combined $domain.pem file for haproxy
-    cat fullchain.pem privkey.pem > /etc/ssl/private/$domain.pem
+    cat fullchain.pem privkey.pem > $CRT_FOLDER/$domain.pem
 done
 
 # reload haproxy to load the new certificate(s)
